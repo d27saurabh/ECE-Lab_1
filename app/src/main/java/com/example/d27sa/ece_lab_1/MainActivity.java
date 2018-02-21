cea1ed9e10f30358720079ccc03661361285bf5f
d27saurabh <d27saurabh@gmail.com>
2018-02-20 21:05:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/d27sa/ece_lab_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.d27sa.ece_lab_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
